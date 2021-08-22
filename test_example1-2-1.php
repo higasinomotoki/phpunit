1a48fad78f2613bcb5fa6cd0f6c1dd3c0200d5f9
miyagawatakashi <miyagawa@hanjou.co.jp>
2021-08-22 22:17:41 +0900

    実行した時コンソールにどんな表示がされるかのテスト

---
+++ test_example1-2-1.php
@@ -0,0 +1,14 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class MyTestCase extends TestCase
+{
+    public function test_test1(): void
+    {
+        echo "test1¥n";
+    }
+    public function test_test2(): void
+    {
+        echo "test2¥n";
+    }
+}
